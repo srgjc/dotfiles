d9167f65fa468b84145779b1f1886e3160015605
Sergio Jimenez <sergio.jimenezcedron@icloud.com>
2026-05-03 12:38:45 +0200

    Refactor services config

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -19,19 +19,36 @@
     dates = "monthly";
   };
 
-  # Load nvidia driver for Xorg and Wayland
-  services.xserver.videoDrivers = ["nvidia"];
 
-  services.upower.enable = true;
-
-  services.qbittorrent = {
-    enable = true;
-    openFirewall = true;
-  };
-
-  services.jellyfin = {
-    enable = true; # Port 8096
-    openFirewall = true;
+  services = {
+    qbittorrent = {
+      enable = true;
+      openFirewall = true;
+    };
+    jellyfin = {
+      enable = true; # Port 8096
+      openFirewall = true;
+    };
+    xserver.videoDrivers = ["nvidia"]; # Load nvidia driver for Xorg and Wayland
+    upower.enable = true;
+    xserver.enable = true; # Enable the X11 windowing system.
+    xserver.xkb = { # Configure keymap in X11
+      layout = "us";
+      variant = "intl";
+    };
+    printing.enable = true; # Enable CUPS to print documents.
+    pulseaudio.enable = false; # Enable sound with pipewire.
+    mullvad-vpn.enable = true;
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      #jack.enable = true; If you want to use JACK applications, uncomment this
+      #media-session.enable = true; Default for now
+    };
+    # openssh.enable = true; Enable the OpenSSH daemon.
+    # xserver.libinput.enable = true; Enable touchpad support (enabled default in most desktopManager).
   };
 
   # Hardware Settings
@@ -113,39 +130,8 @@
     LC_TELEPHONE = "de_DE.UTF-8";
     LC_TIME = "de_DE.UTF-8";
   };
-
-  # Enable the X11 windowing system.
-  # You can disable this if you're only using the Wayland session.
-  services.xserver.enable = true;
-
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "intl";
-  };
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound with pipewire.
-  services.pulseaudio.enable = false;
   security.rtkit.enable = true;
-  services.mullvad-vpn.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
 
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.serj = {
@@ -226,11 +212,6 @@
   #   enable = true;
   #   enableSSHSupport = true;
   # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
